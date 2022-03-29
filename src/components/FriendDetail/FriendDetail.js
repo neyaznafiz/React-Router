bd3052f0af87c02892b8df7e7903c8988769d2e8
niiyazobug <niiyaz5555@gmail.com>
2022-03-29 23:10:24 +0600

    useParams

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
+
+const params = useParams()
+
     return (
         <div>
-            <h2>This is Detail of a Bondhu</h2>
+            <h2>This is Detail about : {params.friendId}</h2>
         </div>
     );
 };
