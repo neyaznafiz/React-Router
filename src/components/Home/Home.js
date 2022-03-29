e3322bff9d7ee4d9149da3fe898e6a51eb5ca415
niiyazobug <niiyaz5555@gmail.com>
2022-03-30 00:55:50 +0600

    display post details with nasted routing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>this is home</h2>
+            <h2>This is home</h2>
             <p>Place for goom</p>
         </div>
     );
---
+++ src/components/PostDetail/PostDetail.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const PostDetail = () => {
+
+    const { postId } = useParams()
+
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/posts/${postId}`
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setPost(data))
+    }, [postId])
+
+    return (
+        <div>
+            <h4>This is post detail for: {postId}</h4>
+            <h5>{post.title}</h5>
+            <p>{post.body}</p>
+        </div>
+    );
+};
+
+export default PostDetail;
