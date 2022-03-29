ceba215fd5eb130c565bbeb199b353be8da7eee5
niiyazobug <niiyaz5555@gmail.com>
2022-03-29 20:38:58 +0600

    friend detail component added

---
+++ src/components/FriendDetail/FriendDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendDetail = () => {
+    return (
+        <div>
+            <h2>This is Detail of a Bondhu</h2>
+        </div>
+    );
+};
+
+export default FriendDetail;
